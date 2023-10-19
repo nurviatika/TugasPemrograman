baa084f242783f99a8fa2486e265d3d5fa3affa5
siti nurviatika <114561174+nurviatika@users.noreply.github.com>
2023-10-19 19:32:09 +0700

    Add files via upload
    membuat variabel dengan nama restaurant bertipe object

---
+++ TugasPemrograman3.js
@@ -0,0 +1,44 @@
+console.log("------------------------")
+console.log("Nama   : Siti nurviatika")
+console.log("Nim    : 20220040281")
+console.log("Kelas  : TI22A")
+console.log("-------------------------")
+
+// variabel bertipe objek
+let restaurant = {
+    name: "Resto A",
+    City: "Jogjakarta",
+    favoriteDrink: "Thai Tea",
+    favoriteFood: "Sate",
+    isVariabel: false
+};
+
+// Variabel dengan tipe array
+let restaurants = [
+    { 
+        name: "Resto A",
+        city: "Jogjakarta",
+        favoriteDrink: "Thai Tea",
+        favoriteFood: "Sate",
+        isVegan: false
+    },
+    {
+        name: "Resto B",
+        city: "Bandung",
+        favoriteDrink: "Macha",
+        favoriteFood: "Cuangki Bakso",
+        isVegan: true
+
+    },
+    {
+        name: "Resto C",
+        city: "Jakarta",
+        favoriteDrink: "Es Lemone",
+        favoriteFood: "Mie Ayam",
+        isVegan: false
+
+    }
+];
+
+console.log(restaurant);
+console.log(restaurants);
